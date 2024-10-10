2e84d7ad72fc557857d8fe76e2189af384521b3f
Mike Valeriano <mkvlrn@gmail.com>
2024-10-10 10:36:59 -0300

    chore: update

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,15 @@
 import { AwsCliCheck } from "~/apps/aws-cli";
 import { BunCheck } from "~/apps/bun";
 import { DenoCheck } from "~/apps/deno";
-import { GoCheck } from "~/apps/go";
-import { GoToolsCheck } from "~/apps/go-tools";
 import { NerdFontsCheck } from "~/apps/nerd-fonts";
 import { OhMyPoshCheck } from "~/apps/oh-my-posh";
 
 await new AwsCliCheck().update();
 await new BunCheck().update();
 await new DenoCheck().update();
-await new GoCheck().update();
-await new GoToolsCheck().update();
 await new NerdFontsCheck().update();
 await new OhMyPoshCheck().update();
 
+// await new GoCheck().update();
+// await new GoToolsCheck().update();
 // await new TerraformCheck().update();
